2d9bb94d8d82c27f64265b3f06fb258a20165301
VenkataM18 <venkata83.maddu@gmail.com>
2023-05-10 19:17:52 +0530

    commit it the local and then push it to the hit hub

---
+++ src/main/java/com/example/test/logisticsdemoprjservice/LogisticsDemoPrjApplication.java
@@ -0,0 +1,13 @@
+package com.example.test.logisticsdemoprjservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogisticsDemoPrjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LogisticsDemoPrjApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/test/logisticsdemoprjservice/LogisticsDemoPrjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.test.logisticsdemoprjservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogisticsDemoPrjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

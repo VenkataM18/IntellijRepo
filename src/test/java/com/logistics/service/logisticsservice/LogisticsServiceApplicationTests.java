f5b18da6f0bc6b9ad26d892e36c8c91ea1f75273
VenkataM18 <venkata83.maddu@gmail.com>
2023-05-10 18:50:12 +0530

    Committed the changes first time

---
+++ src/test/java/com/logistics/service/logisticsservice/LogisticsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.logistics.service.logisticsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogisticsServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
